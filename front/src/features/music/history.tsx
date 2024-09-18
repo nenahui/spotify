5b8a53ad6f066ed16873b0e3a43cff192b4bdb79
nenahui <kanatraccoon@gmail.com>
2024-09-18 15:34:44 +0600

    Removed console logs and displayed text if the listening history is empty

--- front/src/features/music/history.tsx
+++ front/src/features/music/history.tsx
@@ -29,13 +29,14 @@ export const History: React.FC = () => {
     return <Loader absoluteCenter background={false} />;
   }
 
-  console.log(history);
   return (
     <div className={'px-4 py-6 lg:px-8'}>
       <div className={'flex flex-col gap-1.5'}>
-        {history.map((track) => (
-          <HistoryCard track={track.track} key={track._id} datetime={track.datetime} />
-        ))}
+        {history.length === 0 ? (
+          <p className={'text-center text-muted-foreground text-sm'}>Listening history is empty</p>
+        ) : (
+          history.map((track) => <HistoryCard track={track.track} key={track._id} datetime={track.datetime} />)
+        )}
       </div>
     </div>
   );
